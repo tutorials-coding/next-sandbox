a6436c0862e25e71faf07a2496881b851e7d6376
Vadim <korobeinikovv@mail.ru>
2021-04-16 12:27:19 +0300

    Add code snippets

---
+++ snippets/next/static-site-generation-content.js
@@ -0,0 +1,20 @@
+function Blog({ posts }) {
+  // Render posts...
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://.../posts')
+  const posts = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default Blog
---
+++ snippets/sapper/server-side-rendering.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	export async function preload(page, session) {
+		const { slug } = page.params;
+
+		const res = await this.fetch(`blog/${slug}.json`);
+		const article = await res.json();
+
+		return { article };
+	}
+</script>
+
+<script>
+	export let article;
+</script>
+
+<h1>{article.title}</h1>
